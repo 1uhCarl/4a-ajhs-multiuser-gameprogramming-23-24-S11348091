5859de3333ba0929ee69fcbebb66d53271b92ec5
1uhCarl <111576320+1uhCarl@users.noreply.github.com>
2023-12-19 14:14:12 -0500

    projectMethod.cs

---
+++ multiUserGameProgramming/5AGameMethod/projectmethod.cs
@@ -0,0 +1,38 @@
+// Carlton Brinkley, Game Method, v0.01
+using Stystem;
+class BasketballGame 
+{
+    static void Main()
+    {
+        int totalShots = 5 ;
+        int shotsMade = 0:
+
+        Console.Writeline("Welcome to the Basketball Shooting Game!");
+        Console.Writeline("You have " + totalShots + "Shots. Good luck!");
+        for (int i = 0; i < totalShots; i++)
+        
+            Console.Writeline("Press 'S' to shoot.")
+            ConsoleKeyInfo key = Console.Readkey();
+            if (key.KeyChar == 'S'  key.KeyChar == 's')
+    }   
+                Random random = new Random();
+                int shotResult = random.Next (0, 2); // 0 represents a miss, 1 represents a make
+                if (shotResults == 1)
+                {
+                    Console.WriteLine("\nGreat shot! You made it");
+                    shotsMade++;
+                }
+                else
+                {
+                    Console.WriteLine("\n0ops! You missed this shot."):
+                }
+            
+                
+                
+
+
+        
+    
+    
+    
+}
